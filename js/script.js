192fb24a6c0138d22e0256cb00764922ba787af3
Dariusz Rosiek <daro.krk@interia.pl>
2018-01-02 14:47:16 +0100

    Added basic html in index.html, added script in js/script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+var getButtons = document.getElementsByClassName('button');
+
+for (var i = 0; i < getButtons.length; i++) {
+	alert(getButtons[i].innerText);
+}
+
+//window.onload = alert(getButtons[i].innerText);
